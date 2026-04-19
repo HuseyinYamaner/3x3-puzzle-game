5c457e87e44011445deb8125e5d8a213c3629739
Huseyin Yamaner <yamanerhuseyin35@gmail.com>
2026-04-19 21:46:32 +0300

    Fonksiyonlar
    Çalışacak fonksiyonların hatlarının oluşturulması

---
+++ sliding_game.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+void randomize()
+{
+	
+}	
+
+void ata()
+{
+	
+	
+}
+
+void ciz()
+{
+	
+}
+
+int bitir()
+{
+	
+}
+
+void oyna()
+{
+	
+}
+
+int main()
+{
+	int konum_i=2, konum_j=2;
+	srand(time(NULL));
+	
+	int dizi[3][3];
+	int gecici[8]={1,2,3,4,5,6,7,8};
+	
+	ata(dizi,gecici);
+	ciz(dizi);
+	
+}
